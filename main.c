369996e6eb1be7a0ac1b3768f7e9e2657030d1ec
Kauan-Azevedo <kauan.azevedo@fontespromotora.com.br>
2023-04-18 16:16:58 -0300

    feat: mysql connection + Create Read

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 // #include <windows.h> // SE WINDOWS
 #include <mysql/mysql.h>
 
+#define MAX_STRING 300
 typedef struct
 {
-    int id;
     char nome[50];
     char endereco[100];
     char cep[50];
@@ -15,149 +15,145 @@ typedef struct
 
 typedef struct
 {
-    int id;
     char nome[50];
     char endereco[150];
     char cep[50];
-
-    int id_hospital;
 } Clinica;
 
 typedef struct
 {
-    int id;
     char nome[150];
     char email[150];
     char cpf[50];
     char telefone[50];
     char endereco[150];
     char especialidade[50];
-
-    int id_clinica;
 } Medico;
 
 typedef struct
 {
-    int id;
     char nome[150];
     char email[150];
     char cpf[50];
     char telefone[50];
     char endereco[150];
-
-    int id_clinica;
-    int id_medico;
-    int id_doenca;
 } Paciente;
 
 typedef struct
 {
-    int id;
     char nome[50];
     char descricao[100];
     int gravidade;
-
-    int id_paciente;
 } Doenca;
 
 // void salvarHospital(Hospital hospital)
-void salvarHospital(int id, char nome[], char endereco[], char cep[])
+void salvarHospital(char nome[], char endereco[], char cep[])
 {
-    FILE *arquivo = fopen("hospitais.csv", "a");
-    fprintf(arquivo, "%d;%s;%s;%s\n", id, nome, endereco, cep);
-    fclose(arquivo);
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    char *server = "localhost";
+    char *user = "root";
+    char *password = "root";
+    char *database = "sistema_hospital";
+
+    conn = mysql_init(NULL);
+
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    char query[1000];
+    sprintf(query, "INSERT INTO Hospital (nome, endereco, cep) VALUES ('%s', '%s', '%s')", nome, endereco, cep);
+
+    if (mysql_query(conn, query))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    mysql_close(conn);
+
+    mysql_close(conn);
 }
 
-Hospital buscarHospital(int id)
+Hospital listarHospitais()
 {
-    FILE *arquivo = fopen("hospitais.csv", "r");
-    Hospital hospital;
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
 
-    while (fscanf(arquivo, "%i;%[^;];%[^;];%s\n", &hospital.id, hospital.nome, hospital.endereco, hospital.cep) != EOF)
+    char *server = "localhost";
+    char *user = "root";
+    char *password = "root";
+    char *database = "sistema_hospital";
+
+    conn = mysql_init(NULL);
+
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
     {
-        if (hospital.id == id)
-        {
-            fclose(arquivo);
-            return hospital;
-        }
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
     }
-    fclose(arquivo);
-    hospital.id = -1;
-    return hospital;
+
+    mysql_query(conn, "SELECT * FROM Hospital");
+
+    res = mysql_store_result(conn);
+
+    printf("\n+----- Hospitais -----+\n\n");
+    while ((row = mysql_fetch_row(res)) != NULL)
+    {
+        printf("Id: %s,\nNome: %s,\nEndereco: %s,\nCEP: %s\n\n", row[0], row[1], row[2], row[3]);
+    }
+    printf("+----- Fim - Hospitais -----+\n");
+
+    mysql_free_result(res);
+
+    mysql_close(conn);
 }
 
 int main()
 {
-    MYSQL *conexao = mysql_init(NULL);
-
-    if (!mysql_real_connect(conexao, "localhost", "root", "root", "sistema-hospital", 0, NULL, 0))
-    {
-        fprintf(stderr, "%s\n", mysql_error(conexao));
-        exit(1);
-    }
-    else
-    {
-        printf("Conectado ao banco de dados!\n");
-        sleep(2);
-    }
-
     int escolha;
-    char temp[1];
+    char temp;
 
     printf("Bem-vindo,\no que deseja fazer?\n");
-    do
+inicio:
+    // system("clear");
+    printf("0 - Sair\n1 - Registrar Hospital\n2 - Listar Hospitais\nEscolha: ");
+    scanf("%i", &escolha);
+    if (escolha == 0)
     {
-        system("clear");
-        printf("0 - Sair\n1 - Registrar Hospital\n2 - Buscar Hospital\n3 - Atualizar Hospital\n4 - Excluir Hospital\nEscolha: ");
-        scanf("%i", &escolha);
-        if (escolha == 0)
-        {
-            system("cls");
-            printf("Saindo...");
-        }
-        else if (escolha == 1)
-        {
-            int id = 0;
-            char nome[50];
-            char endereco[100];
-            char cep[50];
+        // system("clear");
+        printf("\nSaindo...");
+    }
+    else if (escolha == 1)
+    {
+        char nome[50];
+        char endereco[100];
+        char cep[50];
 
-            system("clear");
+        printf("(Hospital)Digite o Nome: ");
+        scanf("%c", &temp);
+        scanf("%[^\n]", nome);
+        printf("(Hospital)Digite o Endereco: ");
+        scanf("%c", &temp);
+        scanf("%[^\n]", endereco);
+        printf("(Hospital)Digite o CEP: ");
+        scanf("%c", &temp);
+        scanf("%s", cep);
 
-            printf("(Hospital)Digite o Nome:");
-            scanf("%c", temp);
-            scanf("%[^\n]", nome);
-            printf("(Hospital)Digite o Endereco:");
-            scanf("%c", temp);
-            scanf("%[^\n]", endereco);
-            printf("(Hospital)Digite o CEP:");
-            scanf("%c", temp);
-            scanf("%s", cep);
-
-            salvarHospital(id, nome, endereco, cep);
-                }
-        else if (escolha == 2)
-        {
-            int id = 0;
-
-            printf("Digite o ID: ");
-            scanf("%i", &id);
-
-            Hospital h = buscarHospital(id);
-            if (h.id == -1)
-            {
-                printf("\n\nHospital nao encontrado\n");
-            }
-            else
-            {
-                sleep(1);
-                printf("Hospital encontrado");
-                sleep(1);
-                printf("\nHospital:\nId: %i\nNome: %s\nEndereco: %s\nCep: %s\n\n", h.id, h.nome, h.endereco, h.cep);
-            }
-            escolha = 0;
-        }
-    } while (escolha != 0);
+        salvarHospital(nome, endereco, cep);
+        goto inicio;
+    }
+    else if (escolha == 2)
+    {
+        listarHospitais();
+        goto inicio;
+    }
 
     return 0;
 }
